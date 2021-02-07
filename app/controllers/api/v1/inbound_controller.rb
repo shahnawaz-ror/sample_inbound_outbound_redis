3102f108004e768c8447161e826a5192a2633c7a
mohammad shahnawaz <shahnawaz.spaceo@gmail.com>
2021-02-07 15:46:29 +0530

    added

--- app/controllers/api/v1/inbound_controller.rb
+++ app/controllers/api/v1/inbound_controller.rb
@@ -4,7 +4,8 @@ class Api::V1::InboundController < ApplicationController
 	#Inbound sms need to authenticate first
 	def sms
 		logger.info "#{ENV.fetch("REDIS_URL")}"
-		redis = Redis.new(host: ENV.fetch("REDIS_URL"))
+		# redis = Redis.new(host: ENV.fetch("REDIS_URL"))
+		redis = Redis.current
 		if ["STOP","STOP\n","STOP\r", "STOP\r\n"].include? params[:text]
 			redis.set("#{@account.username+"_#{params[:from]}/#{params[:to]}"}","#{params[:from]}/#{params[:to]}",ex: 4.hours)
 		end
