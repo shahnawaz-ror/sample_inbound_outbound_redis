c4fa4b06c9d69d59dcd36acc74000b7ee66fe4e2
mohammad shahnawaz <shahnawaz.spaceo@gmail.com>
2021-02-07 15:46:24 +0530

    added

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,3 @@
-redis_host = Rails.application.secrets.redis && Rails.application.secrets.redis['host'] || 'localhost'
-redis_port = Rails.application.secrets.redis && Rails.application.secrets.redis['port'] || 6379
-
-# The constant below will represent ONE connection, present globally in models, controllers, views etc for the instance. No need to do Redis.new everytime
-REDIS = Redis.new(host: redis_host, port: redis_port.to_i)
+Redis.current = Redis.new(url:  ENV['REDIS_URL'],
+                          port: ENV['REDIS_PORT'],
+                          db:   ENV['REDIS_DB'])
